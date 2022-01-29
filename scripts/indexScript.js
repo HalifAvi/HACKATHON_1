314d223c89181282083cd70c0c6f47cb462296a8
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-01-29 20:08:18 +0200

    icon

---
+++ scripts/indexScript.js
@@ -0,0 +1,38 @@
+let puzzleIcon = document.querySelector('img#puzzleImage');
+
+
+const changePuzzleIconBackgroundColor = () => {
+
+    puzzleIcon.setAttribute('class','bg-yellow');
+    console.log(puzzleIcon)
+}
+
+
+const displayNextPage = () => {
+
+    console.log("avi");
+
+}
+
+ 
+puzzleIcon.addEventListener('click', displayNextPage);
+
+console.log(puzzleIcon)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
